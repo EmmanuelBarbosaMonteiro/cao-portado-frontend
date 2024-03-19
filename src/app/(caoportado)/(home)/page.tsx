3c2d56e910ebc0e00027ce9d454aebce0517cafa
Emmanuel <emmanuel@MacBook-Air-de-Emmanuel.local>
2024-03-18 22:26:20 -0300

    refactor: Refactored home page

--- src/app/(caoportado)/(home)/page.tsx
+++ src/app/(caoportado)/(home)/page.tsx
@@ -1,28 +1,9 @@
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Caoportado | Home',
+  title: 'Home  | Caoportado',
 }
 
 export default async function Home() {
-  // await new Promise((resolve) => setTimeout(resolve, 5000))
-
-  return (
-    <div className="px-4 pt-8 text-zinc-800">
-      <h1 className="flex items-center justify-start gap-1 text-3xl font-bold before:flex before:h-8 before:w-0.5 before:bg-green-700">
-        Caoportado
-      </h1>
-      <h2 className="flex items-center justify-start py-4">
-        O seu caozinho em boas mãos!
-      </h2>
-      <div className="flex flex-col items-start justify-start gap-4">
-        <button className="w-full rounded-md bg-green-600 px-4 py-2 font-normal hover:bg-green-700">
-          Login
-        </button>
-        <button className="w-full rounded-md bg-green-600 px-4 py-2 font-normal hover:bg-green-700">
-          Cadastro
-        </button>
-      </div>
-    </div>
-  )
+  return <h1>Home</h1>
 }
